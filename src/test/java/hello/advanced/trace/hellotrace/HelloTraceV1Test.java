6f1f0966595eb19cb148e20b86bec3b7f18ac1ba
Bombo <bombo96@chungwoon.ac.kr>
2023-04-11 02:31:15 +0900

    간단한 로그 추적기 테스트

---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV1Test {
+
+    @Test
+    @DisplayName("로그 시작 -> 정상 종료")
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    @DisplayName("로그 시작 -> 예외 발생")
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+}
