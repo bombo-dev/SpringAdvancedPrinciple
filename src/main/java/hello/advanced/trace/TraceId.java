a7fc266c46881521f228eb2102aa43dfc5f1bd19
Bombo <bombo96@chungwoon.ac.kr>
2023-04-11 02:31:07 +0900

    로그 추적기 생성

---
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,47 @@
+package hello.advanced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    // LogId 줄이기.
+    private String createId() {
+        String id = UUID.randomUUID().toString();
+        int lastIndex = id.indexOf('-');
+        id = id.substring(0, lastIndex);
+        return id;
+    }
+
+    public TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
