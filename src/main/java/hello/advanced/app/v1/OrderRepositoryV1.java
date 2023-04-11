aa1db77c9b03bd2eedc3b20606e867138fbc0914
Bombo <bombo96@chungwoon.ac.kr>
2023-04-12 02:07:20 +0900

    로그 추적기 저장소, 서비스에도 수동 적용

--- src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
+++ src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -7,19 +9,38 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class OrderRepositoryV1 {
 
+    private final HelloTraceV1 trace;
+
     public void save(String itemId) {
         // 저장 로직
-        if (itemId.equals("ex")) {
-            throw new IllegalStateException("예외 발생!");
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+            trace.end(status);
+        } catch (IllegalStateException e) {
+            trace.exception(status, e);
+            throw e; // 예외를 던지지 않으면 예외를 먹어버리기 때문에, 다시 던져 줌
         }
-        sleep(1000);
     }
 
     private void sleep(int millis) {
+
+        TraceStatus status = null;
         try {
+            status = trace.begin("OrderRepository.sleep()");
             Thread.sleep(millis);
-        } catch (InterruptedException e) { // 체크 예외 발생
+            trace.end(status);
+        } catch (IllegalStateException e) {
+            trace.exception(status, e);
+            throw e; // 예외를 던지지 않으면 예외를 먹어버리기 때문에, 다시 던져 줌
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+
     }
 }
--- src/main/java/hello/advanced/app/v1/OrderServiceV1.java
+++ src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +10,19 @@ import org.springframework.stereotype.Service;
 public class OrderServiceV1 {
 
     private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
 
     public void orderItem(String itemId) {
-        orderRepository.save(itemId);
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (IllegalStateException e) {
+            trace.exception(status, e);
+            throw e; // 예외를 던지지 않으면 예외를 먹어버리기 때문에, 다시 던져 줌
+        }
     }
+
+
 }
