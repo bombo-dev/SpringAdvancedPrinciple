a25a0fcdabb083a689b2369b9c65446cc544860f
Bombo <bombo96@chungwoon.ac.kr>
2023-04-14 00:01:17 +0900

    [쓰레드 동시성 문제 테스트 동시성 문제 발생 O]

--- src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -22,8 +22,11 @@ public class FieldServiceTest {
         threadB.setName("thread-B");
 
         threadA.start();
-        sleep(2000); // 동시성 문제가 발생하지 않음.
+//        sleep(2000); // 동시성 문제가 발생하지 않음.
+        sleep(100); // 동시성 문제가 발생
+        // threadA는 최소 1초가 지나야 종료가 되는데, threadA가 종료되지 않은 상태에서 threadB 실행
         threadB.start();
+        // 서로가 같은 critical section 에 있는 값을 사용하게 됨, 즉 threadB가 nameStore 를 변경하고, threadA는 변경된 값을 반환함.
 
         sleep(3000); // 메인 쓰레드가 종료하지 않도록 대기
         log.info("main exit");
