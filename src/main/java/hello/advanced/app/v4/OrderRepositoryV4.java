44efbb411340ac2f91b5c5132b9e9eef0fb96227
Bombo <bombo96@chungwoon.ac.kr>
2023-04-14 20:39:33 +0900

    [기존 로그 추적기에 템플릿 메서드 패턴 적용 및 테스트]

---
+++ src/main/java/hello/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,44 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+        // 저장 로직
+        AbstractTemplate<Void> template = new AbstractTemplate<Void>(trace) {
+            @Override
+            protected Void call() {
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+        template.execute("OrderRepository.save()");
+    }
+
+    private void sleep(int millis) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<Void>(trace) {
+            @Override
+            protected Void call() {
+                try {
+                    Thread.sleep(millis);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return null;
+            }
+        };
+        template.execute("OrderRepository.sleep()");
+    }
+}
