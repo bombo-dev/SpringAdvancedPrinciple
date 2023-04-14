4fb4bf7cb7c2d1db98617e33d9e51d5bf880c7ff
Bombo <bombo96@chungwoon.ac.kr>
2023-04-14 22:01:45 +0900

    [전략 패턴 리팩토링 테스트, Setter 로 인한 교체는 싱글톤 패턴 사용 시 동시성 이슈]

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -44,6 +45,27 @@ public class ContextV1Test {
         context1.execute();
 
         ContextV1 context2 = new ContextV1(new StrategyLogic2());
+        context2.execute();
+    }
+
+    @Test
+    void strategyV2() {
+        // 인터페이스에 함수가 하나이므로 람다로 생성 가능
+        Strategy strategyLogic1 = () -> log.info("비즈니스 로직 1 실행");
+        Strategy strategyLogic2 = () -> log.info("비즈니스 로직 2 실행");
+
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context1.execute();
+        context2.execute();
+    }
+
+    @Test
+    void strategyV3() {
+        // 좀 더 간추리기
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직 1 실행"));
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직 2 실행"));
+        context1.execute();
         context2.execute();
     }
 }
