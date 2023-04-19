30fc928c72680ec211ae3ad2fb58e84d6fe313eb
Bombo <bombo96@chungwoon.ac.kr>
2023-04-19 20:17:50 +0900

    [Jdk 동적 프록시 간단한 테스트]

---
+++ proxy/src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -0,0 +1,37 @@
+package hello.proxy.jdkdynamic;
+
+import hello.proxy.jdkdynamic.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
+
+@Slf4j
+public class JdkDynamicProxyTest {
+
+    @Test
+    void dynamicA() {
+        AInterface target = new AImpl();
+
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+        proxy.call(); // 여기서 handler 의 invoke 에 해당 메서드가 인자로 전달 된다.
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
+    @Test
+    void dynamicB() {
+        BInterface target = new BImpl();
+
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
+        proxy.call(); // 여기서 handler 의 invoke 에 해당 메서드가 인자로 전달 된다.
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+}
