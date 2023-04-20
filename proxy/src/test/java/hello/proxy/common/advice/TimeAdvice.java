05e4b82db0077b5f6b328e619db442e7629db64e
Bombo <bombo96@chungwoon.ac.kr>
2023-04-20 18:59:00 +0900

    [Advice 를 적용한 프록시]

---
+++ proxy/src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,22 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        Object result = invocation.proceed(); // ProxyFactory 를 생성할 때 target 정보를 넘겨서 여기서는 안 넘긴다.
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
